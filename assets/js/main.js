88a7b027784fb312e8e4d9252745130ca353bccd
Darrell-MultiCloud <115391193+Darrell-MultiCloud@users.noreply.github.com>
2023-12-17 12:25:51 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,3 +185,41 @@ sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100})
 sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {origin: 'left'})
 sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {origin: 'right'})
 sr.reveal(`.qualification__content, .services__card`, {interval: 100})
+
+/*=============== EMAIL JS ===============*/
+
+emailjs.init('MFkjnWAY21tIWPRE_')
+
+ 
+
+window.onload = function() {
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+
+        // console.log("submitted form");
+
+        event.preventDefault();
+
+        // generate a five digit number for the contact_number variable
+
+        // this.contact_number.value = Math.random() * 100000 | 0;
+
+        // these IDs from the previous steps
+
+        emailjs.sendForm('service_pf47f93','template_su0a3zf', '#contact-form', 'MFkjnWAY21tIWPRE_')
+
+            .then(function() {
+
+                console.log('SUCCESS!');
+
+                document.getElementById('contact-form').reset();
+
+            }, function(error) {
+
+                console.log('FAILED...', error);
+
+            });
+
+    });
+
+}
